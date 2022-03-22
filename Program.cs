51072fbb6e21aa0ce78b1ab306498e3dc161b4b3
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-03-22 13:13:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasljedivanjeGeometrijsko
+{
+    class GeometrijskoTijelo
+    {
+        double volumen, oplosje;
+        public static int BrojacTijela = 0;
+
+        public GeometrijskoTijelo()
+        {
+            BrojacTijela++;
+        }
+    }
+
+    class Kugla : GeometrijskoTijelo
+    {
+
+    }
+
+    class Kvadar : GeometrijskoTijelo
+    {
+
+    }
+
+    class Kocka : Kvadar
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Kugla x = new Kugla();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Kvadar y = new Kvadar();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Kocka z = new Kocka();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+
+            Console.ReadKey();
+
+        }
+    }
+}
